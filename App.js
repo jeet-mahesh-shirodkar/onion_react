c09b6f88e9c68c01d07faf25540a77654a080b72
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2024-12-21 10:18:34 +0530

    ES Module type

--- App.js
+++ App.js
@@ -10,16 +10,18 @@
  *     </div>
  * </div>
  */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 let parent = React.createElement('div', { id: "parent" },
     [
         React.createElement('div', { id: "child1" },
-            [React.createElement('h1', {}, "child1 h1 tag"),
+            [React.createElement('h1', {}, "I am child1 h1 tag"),
             React.createElement('h2', {}, "child1 h2 tag")
             ]),
         React.createElement('div', { id: "child2" },
-            [React.createElement('h1', {}, "child2 h1 tag"),
-            React.createElement('h2', {}, "child2 h2 tag")
+            [React.createElement('h1', {}, "I am child2 h1 tag"),
+            React.createElement('h2', {}, "I am child2 h2 tag")
             ])]
 );
 console.log(parent, typeof (parent))
