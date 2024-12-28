093a6a1d6f11e2239b143ca1f583a5733a3ea84c
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2024-12-29 01:12:06 +0530

    utils constant

---
+++ src/utils/constant.js
@@ -0,0 +1,3 @@
+export const LOGO_URL = "https://thumbs.dreamstime.com/b/food-delivery-logo-template-vector-icon-illustration-170869600.jpg";
+
+export const IMG_URL = "https://images.pexels.com/photos/10927839/pexels-photo-10927839.jpeg";
