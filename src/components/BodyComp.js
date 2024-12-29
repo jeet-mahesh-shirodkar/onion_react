60a0b822e76915ece3f2501f8347d4434f1f53aa
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2024-12-30 02:13:01 +0530

    filter function button

--- src/components/BodyComp.js
+++ src/components/BodyComp.js
@@ -1,14 +1,26 @@
 import cardList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import {useState} from 'react';
 
 const BodyComp = () =>{
+    const [restaurantList,setRestaurantList] = useState(cardList);
+    //console.log(restaurantList);
     return (
         <div className="bodyComp">
-            <div className="search-container">
-                Search
+            <div className="filter-container">
+                <button 
+                className="filter-btn"
+                onClick={()=>{
+                    let filteredList = cardList.filter((res) =>
+                         res.data.avgRating > 4)
+                    //console.log(filteredList);
+                    setRestaurantList(filteredList);
+                }}>
+                    Filter Top Rated
+                </button>
             </div>
             <div className="res-container">
-              {cardList.map((restaurant)=> (<RestaurantCard key={restaurant.data.id} resData={restaurant}/>))}
+              {restaurantList.map((restaurant)=> (<RestaurantCard key={restaurant.data.id} resData={restaurant}/>))}
             </div>
         </div>
     )
