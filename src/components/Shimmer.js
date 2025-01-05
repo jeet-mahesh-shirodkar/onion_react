ae7a281fdb36cfbb6902baaec8964a2517b35ca1
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2025-01-05 13:09:26 +0530

    SimmerUI added

---
+++ src/components/Shimmer.js
@@ -0,0 +1,22 @@
+const Shimmer = () => {
+    return (
+        <div>
+        <div className="search-container">
+            <div className="filter-btn-shimmer"></div>
+        </div>
+        <div className="shimmer-container">
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+        </div>
+        </div>
+    )
+}
+
+export default Shimmer;
